d1f412af58270ee05e350b63245aa1a0fa56117e
Apfel <32610623+Apfel@users.noreply.github.com>
2019-02-25 02:05:39 +0100

    I hope I fixed this

--- library.go
+++ library.go
@@ -126,11 +126,11 @@ func (d *Device) Close() StatusCode {
 
 // GetFloat gets a floating point value from a device.
 func (d *Device) GetFloat(value FloatValue, length int) (StatusCode, []float32) {
-	var val *C.float
-	code := StatusCode(C.ohmd_device_getf(d.c, C.ohmd_float_value(value), val))
+	var val C.float
+	code := StatusCode(C.ohmd_device_getf(d.c, C.ohmd_float_value(value), &val))
 
 	f := make([]float32, length)
-	for i, v := range (*[1 << 30]C.float)(unsafe.Pointer(val))[:length:length] {
+	for i, v := range (*[1 << 30]C.float)(unsafe.Pointer(&val))[:length:length] {
 		f[i] = float32(v)
 	}
 
@@ -139,25 +139,25 @@ func (d *Device) GetFloat(value FloatValue, length int) (StatusCode, []float32) 
 
 // SetFloat sets a floating point value for a device.
 func (d *Device) SetFloat(value FloatValue, input []float32, length int) StatusCode {
-	var val *C.float
+	var val C.float
 
-	for i, v := range (*[1 << 30]*C.float)(unsafe.Pointer(val))[:length:length] {
+	for i, v := range (*[1 << 30]C.float)(unsafe.Pointer(&val))[:length:length] {
 		f := C.float(input[i])
-		v = &f
+		v = f
 		_ = v // thank you Google
 	}
 
-	return StatusCode(C.ohmd_device_setf(d.c, C.ohmd_float_value(value), val))
+	return StatusCode(C.ohmd_device_setf(d.c, C.ohmd_float_value(value), &val))
 }
 
 // GetInt gets an integer value from a device.
 func (d *Device) GetInt(value IntValue, length int) (StatusCode, []int) {
-	var val *C.int
-	code := StatusCode(C.ohmd_device_geti(d.c, C.ohmd_int_value(value), val))
+	var val C.int
+	code := StatusCode(C.ohmd_device_geti(d.c, C.ohmd_int_value(value), &val))
 
 	// l a z i n e s s
 	f := make([]int, length)
-	for i, v := range (*[1 << 30]C.float)(unsafe.Pointer(val))[:length:length] {
+	for i, v := range (*[1 << 30]C.int)(unsafe.Pointer(&val))[:length:length] {
 		f[i] = int(v)
 	}
 
@@ -166,15 +166,15 @@ func (d *Device) GetInt(value IntValue, length int) (StatusCode, []int) {
 
 // SetInt sets an integer value for a device.
 func (d *Device) SetInt(value IntValue, input []int, length int) StatusCode {
-	var val *C.int
+	var val C.int
 
-	for i, v := range (*[1 << 30]*C.int)(unsafe.Pointer(val))[:length:length] {
+	for i, v := range (*[1 << 30]C.int)(unsafe.Pointer(&val))[:length:length] {
 		f := C.int(input[i])
-		v = &f
+		v = f
 		_ = v // thank you Google
 	}
 
-	return StatusCode(C.ohmd_device_seti(d.c, C.ohmd_int_value(value), val))
+	return StatusCode(C.ohmd_device_seti(d.c, C.ohmd_int_value(value), &val))
 }
 
 // SetData sets direct data for a device.
