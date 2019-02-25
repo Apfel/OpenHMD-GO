583d0b0b832cd9b3b3c68f4533c7ac82ac5fd30d
Apfel <32610623+Apfel@users.noreply.github.com>
2019-02-25 22:44:38 +0100

    I hope it works now

--- library.go
+++ library.go
@@ -126,10 +126,10 @@ func (d *Device) Close() StatusCode {
 }
 
 // GetFloat fetches one float value.
-func (d *Device) GetFloat(value FloatValue, length int) (StatusCode, []float32) {
+func (d *Device) GetFloat(value FloatValue, index, length int) (StatusCode, float32) {
 	floats := make([]float32, length)
-	code := StatusCode(C.ohmd_device_getf(d.c, C.ohmd_float_value(value), (*C.float)(&floats[0]))) // (unsafe.Pointer(&floats))
-	return code, floats
+	code := StatusCode(C.ohmd_device_getf(d.c, C.ohmd_float_value(value), (*C.float)(&floats[index]))) // (unsafe.Pointer(&floats))
+	return code, floats[index]
 }
 
 // GetInt fechtes int values.
