5aba1ab1fe0f97589c74bc6817c27597d4896084
Apfel <32610623+Apfel@users.noreply.github.com>
2019-03-09 20:21:52 +0100

    moved installation script and switched to CMake for travis

---
+++ .travis-deps.sh
@@ -0,0 +1,23 @@
+echo "Installing Dependencies"
+sudo apt update
+sudo apt install libudev-dev libusb-1.0-0-dev libtool -y
+
+echo "Cloning repositories"
+git clone https://github.com/OpenHMD/OpenHMD.git
+git clone https://github.com/OpenHMD/hidapi.git
+
+echo "Installing HIDAPI"
+cd hidapi
+autoreconf --install --verbose --force && ./configure
+make && sudo make install
+cd ..
+
+echo "Installing OpenHMD"
+cd OpenHMD
+mkdir build
+cd build
+cmake .. && make
+sudo make install
+
+cd ..
+rm -rf hidapi OpenHMD
