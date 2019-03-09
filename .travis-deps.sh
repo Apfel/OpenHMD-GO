eb0a529ed28f876edf25da8574db7004a04ead65
Marko <32610623+Apfel@users.noreply.github.com>
2019-03-09 20:51:44 +0100

    Update .travis-deps.sh

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -1,6 +1,6 @@
 echo "Installing Dependencies"
 sudo apt update
-sudo apt install libudev-dev libusb-1.0-0-dev libtool python3 python3-pip ninja-build -y
+sudo apt install libudev-dev libusb-1.0-0-dev libtool python3.6 python3-pip ninja-build -y
 pip3 install setuptools meson
 
 echo "Cloning repositories"
