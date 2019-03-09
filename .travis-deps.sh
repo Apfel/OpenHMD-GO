400460a11f4b41e28c0f1ebaa7c652d0eaf8ca5d
Marko <32610623+Apfel@users.noreply.github.com>
2019-03-09 20:49:13 +0100

    I hate Ubuntu 16.04

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -1,7 +1,7 @@
 echo "Installing Dependencies"
 sudo apt update
 sudo apt install libudev-dev libusb-1.0-0-dev libtool python3 python3-pip ninja-build -y
-pip3 install meson
+pip3 install setuptools meson
 
 echo "Cloning repositories"
 git clone https://github.com/OpenHMD/OpenHMD.git
@@ -15,8 +15,9 @@ cd ..
 
 echo "Installing OpenHMD"
 cd OpenHMD
-autoreconf --install --verbose --force && ./configure
-make && sudo make install
+meson ./build
+ninja -C ./build/
+sudo ninja -C ./build/ install
 cd ..
 
 rm -rf hidapi OpenHMD
